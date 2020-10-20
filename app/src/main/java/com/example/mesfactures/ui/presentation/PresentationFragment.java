f6397eddf0703ece5a9a4961989afea0ad47c163
omarsamb <omarsamb955@gmail.com>
2020-10-20 09:21:54 +0000

    first commit

---
+++ app/src/main/java/com/example/mesfactures/ui/presentation/PresentationFragment.java
@@ -0,0 +1,24 @@
+package com.example.mesfactures.ui.presentation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.mesfactures.R;
+
+public class PresentationFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_presentation, container, false);
+
+        return root;
+    }
+}
