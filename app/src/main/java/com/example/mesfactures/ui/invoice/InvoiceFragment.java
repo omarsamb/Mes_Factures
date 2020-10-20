35051d381e830de7c7b930d4dff20e62fc2704d9
omarsamb <omarsamb955@gmail.com>
2020-10-20 09:34:07 +0000

    first commit

---
+++ app/src/main/java/com/example/mesfactures/ui/invoice/InvoiceFragment.java
@@ -0,0 +1,29 @@
+package com.example.mesfactures.ui.invoice;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.mesfactures.R;
+
+public class InvoiceFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_invoice, container, false);
+
+
+        return root;
+    }
+}
